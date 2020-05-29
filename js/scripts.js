6433a40724fdebe39e924617bf959d0635b3ae58
Maureen <mstienei@gmail.com>
2020-05-29 10:21:50 +0300

    add condition to check if a null response has been submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+//Declare constants
+
 //Find the value of each element in the form
-function validate(){
+function validate() {
   var cc = document.getElementById("CC").value;
   var yy = document.getElementById("YY").value;
   var mm = document.getElementById("MM").value;
   var dd = document.getElementById("DD").value;
   var gender = document.getElementById("Gender").value;
-  alert("I am" +gender+".Born in"+dd+mm+yy+cc)
+  if (cc === "" || yy === "" ||  mm === "" || dd === "" ){
+    alert("Please provide all the required details in the form");
+  }
+  else{
+    //Compute year Code
+    var div=Math.floor(yy/4);
+    var sum=yy+div;
+    var yearCode=sum%7;
+    alert(yearCode);
+}
 };
